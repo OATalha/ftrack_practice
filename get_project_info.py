3a7da59649bddf55ed2acbc0f9fe61672a61b3d5
Talha Ahmed <talha.ahmed@moonbug.com>
2022-12-22 18:19:17 +0800

    Timer changes

--- get_project_info.py
+++ get_project_info.py
@@ -9,16 +9,48 @@ from mytimer import Timer
 
 
 project_keys = [
-    "created_at", "managers", "calendar_events", "color", "disk_id",
-    "full_name", "disk", "children", "timelogs", "end_date",
-    "parent_id", "created_by", "id", "user_security_role_projects",
-    "start_date", "project_id", "project_schema", "metadata",
-    "status", "scopes", "project_schema_id", "parent", "descendants",
-    "thumbnail_id", "review_sessions", "appointments", "link",
-    "review_session_folders", "is_private", "assets", "is_global",
-    "name", "notes", "thumbnail", "assignments", "thumbnail_url",
-    "allocations", "custom_attributes", "created_by_id", "_link",
-    "root", "context_type",
+    "created_at",
+    "managers",
+    "calendar_events",
+    "color",
+    "disk_id",
+    "full_name",
+    "disk",
+    "children",
+    "timelogs",
+    "end_date",
+    "parent_id",
+    "created_by",
+    "id",
+    "user_security_role_projects",
+    "start_date",
+    "project_id",
+    "project_schema",
+    "metadata",
+    "status",
+    "scopes",
+    "project_schema_id",
+    "parent",
+    "descendants",
+    "thumbnail_id",
+    "review_sessions",
+    "appointments",
+    "link",
+    "review_session_folders",
+    "is_private",
+    "assets",
+    "is_global",
+    "name",
+    "notes",
+    "thumbnail",
+    "assignments",
+    "thumbnail_url",
+    "allocations",
+    "custom_attributes",
+    "created_by_id",
+    "_link",
+    "root",
+    "context_type",
 ]
 
 
@@ -26,21 +58,23 @@ def print_all_project_info(stdout=False):
     cred = get_credentials("talha.ahmed")
     outfile = sys.stdout if stdout else io.StringIO()
 
-    t = Timer()
+    t = Timer("Project Query")
     session = ftrack_api.Session(**cred)
-    t.print_elapsed()
+    t.print_elapsed("After session create")
 
-    project_query = 'select {} from Project'.format(','.join(project_keys))
+    project_query = "select {} from Project".format(",".join(project_keys))
     # project_query = 'Project'
     projects = session.query(project_query).all()
-    t.print_elapsed()
+    t.print_elapsed("After project query")
 
     for project in projects:
-        print(project['full_name'] + ':', file=outfile)
+        print(project["full_name"] + ":", file=outfile)
         for key in project_keys:
             print("\t%s = %s" % (key, project[key]), file=outfile)
         print(file=outfile)
-    t.print_elapsed()
+    t.print_elapsed("After printing all info")
+
+    session.close()
 
 
 if __name__ == "__main__":
--- mytimer.py
+++ mytimer.py
@@ -2,11 +2,14 @@ import timeit
 
 
 class Timer:
-    def __init__(self):
+    def __init__(self, name="Timer"):
+        self.name = name
         self.start()
         self.num = 0
 
-    def start(self):
+    def start(self, name=None):
+        if name is not None:
+            self.name = name
         self.start_time = timeit.default_timer()
 
     def elapsed(self):
@@ -16,5 +19,9 @@ class Timer:
     def print_elapsed(self, checkpoint=None):
         elapsed = self.elapsed()
         if checkpoint is None:
-            checkpoint = 'checkpoint {}'.format(self.num)
-        print('{} seconds elapsed at {}'.format(elapsed, checkpoint))
+            checkpoint = "checkpoint {}".format(self.num)
+        print(
+            "{}: {} seconds elapsed at {}".format(
+                self.name, elapsed, checkpoint
+            )
+        )
